10ae914445cb138cfefc38bd7df2756e34cc0290
saquettepj <thiago.saquettepj@gmail.com>
2025-10-22 15:16:49 -0300

    Update basePath in next.config.js for production environment
    - Changed basePath from '/portifolio' to an empty string for production to ensure correct asset referencing in deployment.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portifolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   images: { unoptimized: true },
 }
 
