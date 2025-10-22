a96bac31282b926651f26e7046ea779f21fa03b8
saquettepj <thiago.saquettepj@gmail.com>
2025-10-22 15:55:13 -0300

    Update Next.js configuration and image path handling for production deployment
    - Adjusted next.config.js to set basePath and assetPrefix based on the production environment instead of GitHub Actions.
    - Modified index.tsx to dynamically determine basePath for GitHub Pages based on the hostname, ensuring correct asset referencing across environments.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProduction = process.env.NODE_ENV === 'production'
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
 
 const nextConfig = {
   output: 'export',
-  basePath: isGitHubPages ? '/portifolio' : '',
-  assetPrefix: isGitHubPages ? '/portifolio' : '',
+  basePath: isProduction ? '/portifolio' : '',
+  assetPrefix: isProduction ? '/portifolio' : '',
   images: { unoptimized: true },
   trailingSlash: true,
 }
