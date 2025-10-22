e62d5328cc259311f4139e5e2d04b36b0e001c4e
saquettepj <thiago.saquettepj@gmail.com>
2025-10-22 18:56:49 -0300

    refactor(config): restaurar basePath e assetPrefix no Next.js
    - Removido o código comentado para basePath e assetPrefix.
    - Restauradas as condições para definir basePath e assetPrefix com base no ambiente de produção.
    
    Essas alterações melhoram a clareza do código e garantem que as configurações de basePath e assetPrefix sejam aplicadas corretamente, dependendo do ambiente, o que é crucial para o funcionamento adequado da aplicação em diferentes contextos.

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-/*   basePath: isProduction ? '/portifolio' : '',
-  assetPrefix: isProduction ? '/portifolio' : '', */
-  basePath: '/portifolio',
-  assetPrefix: '/portifolio',
+  basePath: isProduction ? '/portifolio' : '',
+  assetPrefix: isProduction ? '/portifolio' : '',
   images: { unoptimized: true },
   trailingSlash: true,
 }
